da121572faba280ee8fba7282c5340ce45705bde
ihumentsev <a.ihumentsev@gmail.com>
2023-04-18 17:49:07 +0200

    update/delete/basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/test-work/">
+    <BrowserRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
